1581bd4ae07e5b3b5fbc8a927ad84cc0b8ebbb15
AbdullahWins <noyononline@gmail.com>
2023-02-28 01:07:43 +0600

    added assistant

--- src/components/Modules/PersonalAssistant.js
+++ src/components/Modules/PersonalAssistant.js
@@ -1,11 +1,73 @@
-import React from 'react';
+import React, { useContext } from "react";
+import { AiContext } from "../../contexts/AiContext";
 
 const PersonalAssistant = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const { output, processRequest, isLoading, setIsLoading } =
+    useContext(AiContext);
+
+  const handleClick = () => {
+    const input = document.getElementById("input")?.value;
+    const prompt =
+      "Act like a personal assistant, provide structured assistance for";
+    setIsLoading(true);
+    processRequest(prompt, input);
+  };
+  return (
+    <section className="bg-whiteMid">
+      <div className="lg:w-1/2 mx-auto my-0 bg-whiteHigh rounded-lg">
+        <section className="flex flex-col">
+          <h1 className="text-3xl font-bold text-blackHigh p-2">
+            Personal Assistant
+          </h1>
+          <div className="flex flex-col gap-2 p-2">
+            <textarea
+              className={`p-3 w-full rounded-sm border-solid border-2 border-whiteLow`}
+              placeholder="Let me assist you with the help of AI"
+              name="blog-input"
+              id="input"
+              cols="5"
+              rows="5"
+            ></textarea>
+            <button
+              className={`btn text-whiteHigh bg-infoColor border-none btn-md rounded-md px-4 ${
+                isLoading ? "bg-mainColor w-44" : "bg-whiteLow w-36"
+              }`}
+              onClick={handleClick}
+            >
+              {isLoading ? (
+                <p>
+                  <span>
+                    <i class="fa-solid fa-spinner fa-spin-pulse"></i>
+                  </span>
+                  <span> Generating... </span>
+                </p>
+              ) : (
+                <p>Create&nbsp;Content</p>
+              )}
+            </button>
+          </div>
+          <div>
+            <p className="font-bold text-xl text-center py-2">
+              {isLoading ? (
+                <div>
+                  <i className="fa-solid fa-arrow-down fa-beat-fade"></i>
+                  <i className="fa-solid fa-arrow-down fa-beat-fade"></i>
+                  <i className="fa-solid fa-arrow-down fa-beat-fade"></i>
+                </div>
+              ) : (
+                <div>
+                  <i className="fa-solid fa-arrow-down"></i>
+                </div>
+              )}
+            </p>
+          </div>
+          <div className="p-4">
+            <p>{output}</p>
+          </div>
+        </section>
+      </div>
+    </section>
+  );
 };
 
 export default PersonalAssistant;
