d091d2e161eaeca523e4877053aca70726ef55db
AbdullahWins <noyononline@gmail.com>
2023-03-01 02:32:49 +0600

    changed the name of the site

--- src/components/Shared/Menu.js
+++ src/components/Shared/Menu.js
@@ -5,7 +5,7 @@ import logo from "../../assets/logo/logo.png";
 const Menu = () => {
   return (
     <div className="flex items-center justify-between p-4 text-xl font-bold text-center text-whiteMid bg-pureBlackColor">
-      <p>Assist</p>
+      <p>ASSIST</p>
       <Link to="https://artsky.io/">
         <img className="h-6" src={logo} alt="" />
       </Link>
