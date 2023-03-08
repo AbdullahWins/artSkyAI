885639ef63b483d6cca0a2131eace8821fc11a4b
AbdullahWins <noyononline@gmail.com>
2023-03-08 16:13:36 +0600

    added new loading screen

--- src/routes/PrivateRouter.js
+++ src/routes/PrivateRouter.js
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import { Navigate, useLocation } from "react-router-dom";
 import { AuthContext } from "../contexts/AuthContext";
+import loadingScreen from "../assets/loading/loading.gif";
 
 const PrivateRoute = ({ children }) => {
   const { user, loading } = useContext(AuthContext);
@@ -8,8 +9,8 @@ const PrivateRoute = ({ children }) => {
   const location = useLocation();
   if (loading) {
     return (
-      <div className="flex items-center justify-center py-6 bg-base-300">
-        <progress className="progress w-56"></progress>
+      <div className="h-screen">
+        <img src={loadingScreen} alt="" />
       </div>
     );
   }
